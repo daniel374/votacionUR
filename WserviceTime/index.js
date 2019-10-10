44575abd8cbedd04fd3336864f314408db7b9e78
daniel374 <damrodriguezlay@unal.edu.co>
2019-10-10 00:30:57 -0500

    ajustes 2

--- WserviceTime/index.js
+++ WserviceTime/index.js
@@ -8,14 +8,23 @@ var request 		= require('request');
 exports.handler = async function (event){
 	var date = new Date();
 	var habilitado = Boolean;
-    var current_hour = date.getHours();
+	var current_hour = date.getHours();
+	var current_date = date.toISOString();
+	var res_date = current_date.split('T');
+	console.log('fecha ', current_date);
 	console.log('time ', current_hour);
-	if (current_hour > 6 && current_hour < 18) {
-		habilitado = true;
+	console.log('fecha sola ',res_date);
+	if (res_date[0] == '2019-10-10' || res_date[0] == '2019-10-11') {
+		if (current_hour > 6 && current_hour < 18) {
+			habilitado = true;
+		} else {
+			habilitado = false;
+		}
 	} else {
 		habilitado = false;
 	}
+	
 	console.log('Habilitado ', habilitado);
 		habilitado = false;
-  	return {current_hour, habilitado}
+  	return {current_hour, habilitado, res_date}
 };
